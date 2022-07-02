6e064dfede4d22f55d35f7a81a7aaaced9f2b067
Paola Basso <paolasbasso@gmail.com>
2022-07-02 14:51:18 -0300

    feat: methods e paths

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+router = express.Router();
+
+router.post("/calendar",(req, res) => {})
+router.post("/menstruation",(req, res) => {})
+router.post("/premenstruation",(req, res) => {})
+router.post("/ovulation",(req, res) => {})
+router.post("/preovulation",(req, res) => {})
